3c214195271abe15139cd3e453c56e389937eecb
jhonathandosreis <jhonathandosreis@hotmail.com>
2021-04-17 03:44:54 -0300

    [JHONATHAN DOS REIS] Classe PedidoResource - Concluida

---
+++ backend/src/main/java/com/ecommerce/backend/resources/PedidoResources.java
@@ -0,0 +1,46 @@
+package com.ecommerce.backend.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.backend.model.Pedido;
+import com.ecommerce.backend.repository.PedidoRepository;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/pedido")
+public class PedidoResources {
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@GetMapping
+	public List<Pedido> get() {
+		return pedidoRepository.findAll();
+	}
+
+	@PostMapping
+	public void post(@RequestBody Pedido pedido) {
+		pedidoRepository.save(pedido);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable("id") Long id) {
+		pedidoRepository.deleteById(id);
+	}
+	
+	@PutMapping
+	public void put(@RequestBody Pedido pedido) {
+		pedidoRepository.save(pedido);
+	}
+}
